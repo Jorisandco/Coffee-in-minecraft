4f2ef8c121e3d1da44822d93fe1df7a17651e917
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-02-06 18:47:00 +0100

    fixed issue where server wouldn't load in plugin

--- src/com/jorisandco/tutorial/Coffee.java
+++ src/com/jorisandco/tutorial/Coffee.java
@@ -1,16 +1,18 @@
 package com.jorisandco.tutorial;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Coffee extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Coffee Plugin Enabled");
 
     }
 
     @Override
     public void onDisable() {
-
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Coffee Plugin crashed");
     }
 }
