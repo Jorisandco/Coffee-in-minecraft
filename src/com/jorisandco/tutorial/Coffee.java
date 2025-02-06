42580300d607ce77a633bc30d38052e406f3cc58
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-02-06 18:35:16 +0100

    added essentials

---
+++ src/com/jorisandco/tutorial/Coffee.java
@@ -0,0 +1,16 @@
+package com.jorisandco.tutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Coffee extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
