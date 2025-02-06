e41c538358eb86b89ace2a622af3398906a323b4
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-02-06 20:02:46 +0100

    fixed coffee comand

--- src/com/jorisandco/tutorial/comands/Debugcomands.java
+++ src/com/jorisandco/tutorial/comands/Debugcomands.java
@@ -1,9 +1,17 @@
 package com.jorisandco.tutorial.comands;
 
+import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Debugcomands implements CommandExecutor {
     @Override
@@ -15,7 +23,13 @@ public class Debugcomands implements CommandExecutor {
 
         //spawn coffee
         if(cmd.getName().equalsIgnoreCase("coffee")) {
-            player.setExp(1);
+            ItemStack poti = new ItemStack(Material.POTION);
+            PotionMeta meta = (PotionMeta)poti.getItemMeta();
+            meta.setDisplayName("coffee");
+            meta.setColor(Color.BLACK);
+            meta.addCustomEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 50, 100), true);
+            poti.setItemMeta((ItemMeta)meta);
+            player.getInventory().addItem(poti);
         }
         return true;
     }
